6ff62b30799fd92ba0d3970c04ff6330e0bef246
Qi Miao <qi.miao@yitu-inc.com>
2019-06-20 10:35:31 +0800

    Circle and Line

--- src/components/PixelShader/CircleAndLine/fragment.glsl
+++ src/components/PixelShader/CircleAndLine/fragment.glsl
@@ -1,3 +1,4 @@
+// Reference: https://blog.csdn.net/candycat1992/article/details/44244549
 uniform vec2 iResolution;
 uniform float iTime;
 uniform vec2 iMouse;
@@ -11,13 +12,18 @@ vec4 Circle(vec2 uv, vec2 p, float radius, vec3 color, float antialias) {
 vec4 Line(vec2 uv, vec2 point0, vec2 point1, float width, vec3 color, float antialias) {
   float k = (point1.y - point0.y) / (point1.x - point0.x);
   float b = point1.y - k * point1.x; 
-  float d = abs(k * point0.x + b)
+  float d = abs(k * uv.x - uv.y + b) / sqrt(k * k + 1.);
+  float t = smoothstep(width/2.0, width/2.0 + antialias, d);
+  return vec4(color, 1. - t);
 }
 
 void main() {
 	 	vec2 pos = (2.0 * gl_FragCoord.xy - iResolution.xy) / min(iResolution.y, iResolution.x);
 
-    vec4 bgColor = vec4(0., 1., 1., 1.);
-    vec4 color = Circle(pos, vec2(0., 0.), .5, vec3(1., 1., 0.), .01);
-    gl_FragColor = mix(bgColor, color, color.a);
+    vec4 bgColor = vec4(0., 0., 0., 1.);
+    vec4 circle = Circle(pos, vec2(0., 0.), .5, vec3(1., 1., 0.), .01);
+    vec4 line = Line(pos, vec2(0., 0.), vec2(.5, .5), .05, vec3(0., 0., 1.), .01);
+    vec4 color = mix(bgColor, circle, circle.a);
+    color = mix(color, line, line.a);
+    gl_FragColor = color;
 }
